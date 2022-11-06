3a28f871876d39dc1ae8027b2d71d20e38820082
StrixSC <talon.strix@gmail.com>
2022-11-06 03:35:04 -0500

    2 New solutions for topkfrequent

--- leetcode/TopKFrequent.py
+++ leetcode/TopKFrequent.py
@@ -17,6 +17,37 @@ class Solution:
 
         return res
 
+    def topKFrequentBucketSort(self, nums: list[int], k: int) -> list[int]:
+        from collections import defaultdict
+        import itertools
+        length = len(nums) + 1
+        buckets = [set() for _ in range(length)]
+        freqs = defaultdict(int)
+        for num in nums:
+            freqs[num]+=1
+
+        for key in freqs.keys():
+            buckets[freqs[key]].add(key)
+
+        res = []
+        buckets = [bucket for bucket in buckets if len(bucket) != 0]
+        for bucket in reversed(buckets):
+            if len(res) < k and len(bucket) != 0:
+                res.append(list(bucket))
+            else:
+                break
+        
+        return list(itertools.chain.from_iterable(res))
+
+
+    def topKFrequentBucketsCounter(self, nums: list[int], k: int) -> list[int]:
+        from collections import Counter
+        import itertools
+        buckets = []*len(nums)
+        for key, freq in Counter(nums).items():
+            buckets[freq].append(key)
+        return list(itertools.chain(*buckets))[:k]
+
 
 def rinput():
 	data = input()
@@ -24,8 +55,8 @@ def rinput():
 
 if __name__ == "__main__":
 	solution = Solution()
-	# res = solution.topKFrequent([1,1,1,2,2,3], k=2)
-	res = solution.topKFrequent([1,2], k=2)
+	# res = solution.topKFrequentBucketSort([1,1,1,2,2,3,3,3,3], k=2)
+	res = solution.topKFrequentBucketsCounter([1,2], k=2)
 	print(res)
 
-# input: 2,7,11,15 9
+# input: 2,7,11,15,9
