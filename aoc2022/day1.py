184485f840c333dc6321f7303eb87c395ba8388e
StrixSC <talon.strix@gmail.com>
2022-12-18 02:22:36 -0500

    Added Advent of Code 2022 day 1

---
+++ aoc2022/day1.py
@@ -0,0 +1,10 @@
+calories, data_path = [], "./data/day1.txt"
+
+with open(data_path, 'r') as f:
+    lines = "".join(f.readlines()).split("\n\n")
+
+for line in lines:
+    calories_clean = line.replace('\n', ',').split(',')
+    calories.append(sum(list(map(int, calories_clean))))
+
+print(calories[max(range(len(calories)), key=calories.__getitem__)])
