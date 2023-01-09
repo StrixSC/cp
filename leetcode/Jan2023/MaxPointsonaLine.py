2785ad515e4a24399daeee86681b035bf94317d5
StrixSC <talon.strix@gmail.com>
2023-01-09 02:27:10 -0500

    added Jan 08 LC challenge

---
+++ leetcode/Jan2023/MaxPointsonaLine.py
@@ -0,0 +1,22 @@
+import math
+from collections import defaultdict
+class Solution:
+    def computeSlope(self, point1, point2):
+        denom = point2[0] - point1[0]
+        if denom == 0: 
+            return math.inf
+        return (point2[1] - point1[1])/denom
+
+    def maxPoints(self, points: List[List[int]]) -> int:
+        if len(points) < 2:
+            len(points)
+        mx = 0
+        for i in range(len(points)):
+            dic = defaultdict(int)
+            for j in range(i+1, len(points)):
+                slope = self.computeSlope(points[j], points[i])
+                dic[slope] += 1
+                mx = max(mx, dic[slope])
+        
+        return mx + 1
+            
