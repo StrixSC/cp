b4e296977af13f44c4d1a76244d0e8ed5de8b2c6
StrixSC <talon.strix@gmail.com>
2024-04-10 02:43:49 -0400

    kermit and prefixarrs :D

---
+++ leetcode/leetbean/leetbean/src/main/java/org/strixsc/ProductOfArrayExceptSelf.java
@@ -0,0 +1,30 @@
+package org.strixsc;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class ProductOfArrayExceptSelf {
+    static class Solution {
+        static public int[] productExceptSelf(int[] nums) {
+            int[] result = new int[nums.length];
+            int prefixProduct = 1;
+            for (int i = 0; i < nums.length; i++) {
+                result[i] = prefixProduct;
+                prefixProduct *= nums[i];
+            }
+
+            int suffixProduct = 1;
+            for (int i = nums.length - 1; i >= 0; i--) {
+                result[i] *= suffixProduct;
+                suffixProduct *= nums[i];
+            }
+            return result;
+        }
+    }
+
+    public static void main(String[] args) {
+            System.out.println(
+                    Arrays.stream(Solution.productExceptSelf(new int[]{1, 2, 3, 4})).mapToObj(String::valueOf).reduce((value, result) -> value + "," + result).stream().collect(Collectors.toList())
+            );
+    }
+}
---
+++ leetcode/leetbean/leetbean/src/main/java/org/strixsc/ValidSudoku.java
@@ -0,0 +1,94 @@
+package org.strixsc;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class ValidSudoku {
+
+    public static class Solution {
+        public static boolean isValidSudoku(char[][] board) {
+            // Traverse rows and check if duplicates are found;
+            Set<Character> set = new HashSet<>();
+            for (int row = 0; row < board.length; row++) {
+                Set<Character> s = new HashSet<>();
+                for (int col = 0; col < board[row].length; col++) {
+                    char lookupValue = board[row][col];
+                    if (lookupValue == '.') {
+                        continue;
+                    }
+
+                    if (s.contains(lookupValue)) {
+                        return false;
+                    }
+
+                    s.add(lookupValue);
+                }
+            }
+
+            // Traverse columns and check if duplicates are found;
+            for (int col = 0; col < board[0].length; col++) {
+                Set<Character> s = new HashSet<>();
+                for (int row = 0; row < board.length; row++) {
+                    char lookupValue = board[row][col];
+                    if (lookupValue == '.') {
+                        continue;
+                    }
+
+                    if (s.contains(lookupValue)) {
+                        return false;
+                    }
+
+                    s.add(lookupValue);
+                }
+            }
+
+            // Traverse each 3x3 block and check if dupes are found
+            for (int row = 0; row < board.length; row += 3) {
+                for (int col = 0; col < board.length; col += 3)
+                {
+                    Set s = new HashSet();
+                    for (int i = 0 ; i < 3; i++)
+                    {
+                        for (int j = 0; j < 3; j++)
+                        {
+                            char lookupValue = board[row + i][col + j];
+                            if (lookupValue == '.')
+                            {
+                                continue;
+                            }
+
+                            if (s.contains(lookupValue))
+                            {
+                                return false;
+                            }
+
+                            s.add(lookupValue);
+                        }
+                    }
+                }
+            }
+
+            return true;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(
+                Solution.isValidSudoku(
+                        new char[][]{
+                                {'.','.','4','.','.','.','6','3','.'},
+                                {'.','.','.','.','.','.','.','.','.'},
+                                {'5','.','.','.','.','.','.','9','.'},
+                                {'.','.','.','5','6','.','.','.','.'},
+                                {'4','.','3','.','.','.','.','.','1'},
+                                {'.','.','.','7','.','.','.','.','.'},
+                                {'.','.','.','5','.','.','.','.','.'},
+                                {'.','.','.','.','.','.','.','.','.'},
+                                {'.','.','.','.','.','.','.','.','.'}
+                        }
+                ));
+    }
+}
